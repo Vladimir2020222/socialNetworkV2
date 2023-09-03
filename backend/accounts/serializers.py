3ee735c2c1bcda6cb9b236673415508d03118593
Vladimir <vova777brawlstars@gmail.com>
2023-09-03 12:52:00 +0300

    sereailzers and services are created

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'password')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = self.Meta.model(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def is_valid(self, *, raise_exception=False):
+        is_valid = super().is_valid(raise_exception=raise_exception)
+        if not is_valid:
+            return False
+        if self._validated_data['password'] is None:
+            if raise_exception:
+                raise ValidationError('password must be set')
+            return False
+        return True
+
+
+class UserSerializerAllData(UserSerializer):
+    class Meta:
+        model = User
+        fields = ('pk', 'username', 'first_name', 'last_name', 'password',
+                  'last_login', 'is_superuser', 'email', 'date_joined')
+
+    def create(self, validated_data):
+        raise NotImplementedError(f'To create User use UserSerializer, not {self.__class__.__name__}')
---
+++ backend/accounts/services.py
@@ -0,0 +1,35 @@
+import datetime
+
+import jwt
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.http import HttpRequest
+from rest_framework.exceptions import AuthenticationFailed
+
+User = get_user_model()
+
+
+def get_user_by_jwt(request: HttpRequest, *, raise_exception=False):
+    token = request.COOKIES.get('jwt')
+    try:
+        if token is None:
+            raise AuthenticationFailed('jwl token is not on COOKIES')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, ['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('ExpiredSignatureError')
+        user = User.objects.get(pk=payload['id'])
+    except (AuthenticationFailed, User.DoesNotExist) as e:
+        if raise_exception:
+            raise e
+        return None
+    return user
+
+
+def generate_new_jwt(user_id):
+    payload = {
+        'id': user_id,
+        'exp': datetime.datetime.now() + datetime.timedelta(weeks=52),
+        'iat': datetime.datetime.now()
+    }
+    return jwt.encode(payload, settings.SECRET_KEY, 'SH256')
