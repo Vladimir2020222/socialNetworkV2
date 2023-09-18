23afb1429e8504a1977bc7b689224fa04770063b
Vladimir <vova777brawlstars@gmail.com>
2023-09-18 20:21:44 +0300

    accountService added

--- frontend/src/app/services/account.service.ts
+++ frontend/src/app/services/account.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { BehaviorSubject } from "rxjs";
+import { User } from "../models/user";
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,53 @@ import { BehaviorSubject } from "rxjs";
 export class AccountService {
   private serverUrl = 'http://127.0.0.1:8000/';
   isLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  userProfile: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
 
   constructor(private http: HttpClient) {}
 
-  updateIsLoggedIn() {
-    this.http.get(this.serverUrl + 'accounts/is_authenticated')
-      .subscribe(value => {
-        console.log(value)})
+  updateIsLoggedIn(): void {
+    this.http.get<boolean>(this.serverUrl + 'accounts/is_authenticated/', {withCredentials: true})
+      .subscribe((value: boolean): void => {
+        this.isLoggedIn.next(value);
+      })
+  }
+
+  updateUserProfile(): void {
+    if (!this.isLoggedIn) {
+      this.userProfile.next(null);
+    }
+    this.http.get<User | null>(this.serverUrl + 'accounts/profile/', {withCredentials: true})
+      .subscribe((value: User | null): void => {
+        this.userProfile.next(value);
+      })
+  }
+
+  logout(): void {
+    this.http.post(this.serverUrl + 'accounts/logout/', null, {withCredentials: true});
+  }
+
+  login(username: string, password: string): void {
+    this.http.post(
+      this.serverUrl + 'accounts/login/',
+      JSON.stringify({username: username, password: password}),
+      {
+        withCredentials: true,
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+    })
+  }
+
+  signup(username: string, password: string, firstName: string, secondName: string): void {
+    this.http.post(
+      this.serverUrl + 'accounts/signup/',
+      JSON.stringify({username: username, password: password, first_name: firstName, second_name: secondName}),
+      {
+        withCredentials: true,
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      }
+    )
   }
 }
