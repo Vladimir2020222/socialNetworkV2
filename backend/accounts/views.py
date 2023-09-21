4a6023835eed1832c98391146bf332e9831f42ab
Vladimir <vova777brawlstars@gmail.com>
2023-09-21 17:34:42 +0300

    custom authentication middleware added, so in views request.user can be used to get a user by jwt

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -41,15 +41,14 @@ class SignOutView(APIView):
 
 class ProfileView(APIView):
     def get(self, request):
-        user = get_user_by_jwt(request)
-        serializer = UserAllDataSerializer(instance=user)
+        serializer = UserAllDataSerializer(request.user)
         return Response(serializer.data)
 
 
 class ChangeUserAva(APIView):
     def post(self, request):
         file = request.FILES.get('ava')
-        user = get_user_by_jwt(request)
+        user = request.user
         if (not file) or (not user):
             return Response({'success': False})
         user.ava = file
