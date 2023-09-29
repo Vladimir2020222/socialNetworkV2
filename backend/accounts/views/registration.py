47e84d828a23799e1c53d1038e3253ce27d03594
Vladimir <vova777brawlstars@gmail.com>
2023-09-29 19:23:01 +0300

    SignUpAPIView and SignInAPIView now return a user object

--- backend/accounts/views/registration.py
+++ backend/accounts/views/registration.py
@@ -1,33 +1,41 @@
 from django.contrib.auth import authenticate
 from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from accounts.serializers import UserSerializer
-from accounts.services import generate_new_jwt, get_user_by_jwt
+from accounts.services import generate_new_jwt
 from accounts.views.mixins import GetUserMixin
 
 
-class SignUpAPIView(GenericAPIView):
+class SignUpAPIView(RetrieveModelMixin, GenericAPIView):
+    serializer_class = UserSerializer
+
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if not serializer.is_valid():
             return Response({'success': False})
-        user = serializer.save()
-        jwt = generate_new_jwt(user.pk)
-        response = Response({'success': True})
+        self.user = serializer.save()
+        jwt = generate_new_jwt(self.user.pk)
+        response = self.retrieve(request)
         response.set_cookie(key='jwt', value=jwt, httponly=True, samesite='none', secure=True)
         return response
 
+    def get_object(self):
+        return self.user
 
-class SignInAPIView(GetUserMixin, GenericAPIView):
+
+class SignInAPIView(GetUserMixin, RetrieveModelMixin, GenericAPIView):
+    serializer_class = UserSerializer
+
     def post(self, request):
         username, password = request.data.get('username'), request.data.get('password')
         user = authenticate(username=username, password=password)
         if user is None:
             return Response({'success': False})
         jwt = generate_new_jwt(user.pk)
-        response = Response({'success': True})
+        response = self.retrieve(request)
         response.set_cookie(key='jwt', value=jwt, httponly=True, samesite='none', secure=True)
         return response
 
