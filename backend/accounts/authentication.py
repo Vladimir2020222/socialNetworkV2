4866f0a43cff368d8955b547ecf20fde20c533a6
Vladimir <vovakrukov082008@gmail.com>
2023-11-06 16:10:25 +0300

    JWTCOOKIESAuthentication returns AnonymousUser() instead of None

--- backend/accounts/authentication.py
+++ backend/accounts/authentication.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.authentication import BaseAuthentication
 from accounts.services import get_user_by_jwt
 
@@ -6,5 +7,5 @@ class JWTCOOKIESAuthentication(BaseAuthentication):
     def authenticate(self, request):
         user = get_user_by_jwt(request._request)
         if user:
-            return user, request._request.COOKIES.get('jwt')
-        return None, None
+            return user or AnonymousUser(), request._request.COOKIES.get('jwt')
+        return AnonymousUser(), None
