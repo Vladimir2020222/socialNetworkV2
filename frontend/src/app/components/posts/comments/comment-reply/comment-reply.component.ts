b0416353dbee782b224cbb2254f824f7ddab40ad
Vladimir <vovakrukov082008@gmail.com>
2023-11-30 21:13:43 +0300

    CommentReplyComponent now displays list of full names of users this reply adressed to

--- frontend/src/app/components/posts/comments/comment-reply/comment-reply.component.ts
+++ frontend/src/app/components/posts/comments/comment-reply/comment-reply.component.ts
@@ -1,11 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommentReply } from "../../../../models/comment-reply";
+import { User } from "../../../../models/user";
+import { AccountService } from "../../../../services/account.service";
 
 @Component({
   selector: 'app-comment-reply',
   templateUrl: './comment-reply.component.html',
   styleUrls: ['./comment-reply.component.css']
 })
-export class CommentReplyComponent {
+export class CommentReplyComponent implements OnInit {
   @Input() reply!: CommentReply;
+  @Input() allReplies!: CommentReply[];
+  replyToNames: string[] = [];
+
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.setReplyToNames();
+  }
+
+  setReplyToNames(): void {
+    const filtered: CommentReply[] = this.allReplies.filter(
+      (reply: CommentReply): boolean => this.reply.reply_to.includes(reply.pk)
+    );
+    const names: string[] = [];
+    filtered.forEach((reply: CommentReply): void => {
+      this.accountService.getUserById(reply.author)
+        .subscribe((author: User | null): void => {
+          if (!author) return;
+          names.push(`${author.first_name} ${author.last_name}`);
+        });
+    });
+    this.replyToNames = names;
+  }
 }
