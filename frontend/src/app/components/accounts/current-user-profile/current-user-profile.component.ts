9e878ef4043629b6cc967fc4816b3e5656028724
vladimirKrukov <vovakrukov082008@gmail.com>
2023-12-21 19:08:13 +0300

    anonymous users now are redirected from accounts/profile to accounts/login

--- frontend/src/app/components/accounts/current-user-profile/current-user-profile.component.ts
+++ frontend/src/app/components/accounts/current-user-profile/current-user-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from "../../../models/user";
 import { AccountService } from "../../../services/account.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-current-user-profile',
@@ -10,12 +11,17 @@ import { AccountService } from "../../../services/account.service";
 export class CurrentUserProfileComponent implements OnInit {
   user: User | null = null;
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {
     this.accountService.userProfile
       .subscribe(
-        (user: User | null): void => {this.user = user}
+	      (user: User | null): void => {
+          this.user = user;
+          if (user === null) {
+            this.router.navigate(['accounts/login']);
+          }
+        }
       )
   }
 }
