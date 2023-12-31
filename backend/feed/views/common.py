fec6e0560d072251f8b9df2cab07e70b149e2392
vladimirKrukov <vovakrukov082008@gmail.com>
2023-12-31 18:19:35 +0300

    LikedByAPIView and DislikedByAPIView not extend new BaseRatedByAPIView class; if requested non-existing model in BaseReatedByAPIView, Http404 is raised instead of exception

--- backend/feed/views/common.py
+++ backend/feed/views/common.py
@@ -1,8 +1,11 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from django.apps import apps
+from rest_framework.views import APIView
+
 from feed.services import rate_obj
 
 
@@ -17,19 +20,20 @@ class RateAPIView(GenericAPIView):
         return Response()
 
 
-class LikedByAPIView(GenericAPIView):
+class BaseRatedByAPIView(GenericAPIView):
+    object_attr_name = None
+
     def get(self, request, model_name):
         pk = request.GET.get('pk')
         model = apps.get_model('feed', model_name)
-        obj = model.objects.get(pk=pk)
-        liked_by = obj.liked_by.values_list('pk', flat=True)
+        obj = get_object_or_404(model, pk=pk)
+        liked_by = getattr(obj, self.object_attr_name).values_list('pk', flat=True)
         return Response(liked_by)
 
 
-class DislikedByAPIView(GenericAPIView):
-    def get(self, request, model_name):
-        pk = request.GET.get('pk')
-        model = apps.get_model('feed', model_name)
-        obj = model.objects.get(pk=pk)
-        disliked_by = obj.dsiliked_by.values_list('pk', flat=True)
-        return Response(disliked_by)
+class LikedByAPIView(BaseRatedByAPIView):
+    object_attr_name = 'liked_by'
+
+
+class DislikedByAPIView(BaseRatedByAPIView):
+    object_attr_name = 'disliked_by'
